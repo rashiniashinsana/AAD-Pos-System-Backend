f7d18d3b0677cd6813434f08e68c5d1d62284c2f
rashiniashinsana <rashinivithanage2@gmail.com>
2024-08-08 01:26:47 +0530

    Complete ItemDTO

--- src/main/java/lk/ijse/gdse68/pos_system_backend/dto/ItemDTO.java
+++ src/main/java/lk/ijse/gdse68/pos_system_backend/dto/ItemDTO.java
@@ -1,8 +1,19 @@
 package lk.ijse.gdse68.pos_system_backend.dto;
 
+import lombok.*;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Setter
+@Getter
 
 public class ItemDTO {
+    private String code;
+    private String name;
+    private Double price;
+    private int qty;
 
 
 }

9f959237a7f2fd054081b7261726329d1b55a7b3
rashiniashinsana <rashinivithanage2@gmail.com>
2024-08-09 14:37:35 +0530

    Create ItemDTO

--- src/main/java/lk/ijse/gdse68/pos_system_backend/dto/ItemDTO.java
+++ src/main/java/lk/ijse/gdse68/pos_system_backend/dto/ItemDTO.java
@@ -2,6 +2,8 @@ package lk.ijse.gdse68.pos_system_backend.dto;
 
 import lombok.*;
 
+import java.math.BigDecimal;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -12,7 +14,7 @@ import lombok.*;
 public class ItemDTO {
     private String code;
     private String name;
-    private Double price;
+    private BigDecimal price;
     private int qty;
 
 
